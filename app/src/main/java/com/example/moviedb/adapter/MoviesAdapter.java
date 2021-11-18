e1c4bcc3f3852b0516bb9de315f55c9eb6927191
gülce <gizemgulce@gmail.com>
2021-11-18 23:02:31 +0300

    feature -rv

--- app/src/main/java/com/example/moviedb/adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/moviedb/adapter/MoviesAdapter.java
@@ -1,5 +1,7 @@
 package com.example.moviedb.adapter;
 
+import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,35 +13,56 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moviedb.R;
+import com.example.moviedb.databinding.RvItemBinding;
+import com.example.moviedb.models.MovieModel;
+import com.example.moviedb.views.mainScreen.SearchScreenFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
     private static final String TAG= "RecyclerViewAdapter";
+    private List<MovieModel> movies;
+    private SearchScreenFragment context;
+
+    public MoviesAdapter(SearchScreenFragment context){
+        this.context= context;
+        this.movies=new ArrayList<>();
+    }
+
+    public void setAdapterMovieList(List<MovieModel> movies){
+        this.movies.addAll(movies);
+        this.notifyDataSetChanged();
+    }
+
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_item,parent,false);
-       ViewHolder holder= new ViewHolder(view);
-        return holder;
+        RvItemBinding binding=RvItemBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         Log.d(TAG,"onBindViewHolder called");
+        MovieModel movieModel=movies.get(position);
+        if(movieModel.getTitle() != null){
+            holder.binding.tvMovieName.setText(movieModel.getTitle());
+            Log.d(TAG, holder.binding.tvMovieName.toString());
+        }else   Log.d(TAG,"null");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-    ImageView movie_image;
-    TextView movie_name;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            movie_image=itemView.findViewById(R.id.iv_movie);
-            movie_name=itemView.findViewById(R.id.tv_movie_name);
+    public class ViewHolder extends RecyclerView.ViewHolder  {
+    RvItemBinding binding;
+        public ViewHolder(RvItemBinding binding) {
+            super(binding.getRoot());
+            this.binding=binding;
         }
     }
 }
--- app/src/main/java/com/example/moviedb/views/mainScreen/SearchScreenFragment.java
+++ app/src/main/java/com/example/moviedb/views/mainScreen/SearchScreenFragment.java
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.moviedb.adapter.MoviesAdapter;
 import com.example.moviedb.databinding.FragmentSearchScreenBinding;
 import com.example.moviedb.models.MovieModel;
 import com.example.moviedb.request.RequestService;
@@ -29,9 +32,9 @@ import retrofit2.Response;
 
 public class SearchScreenFragment extends Fragment {
 
-    private final String TAG = "Custom_Tag_Main";
     private SearchScreenViewModel searchScreenViewModel;
     private FragmentSearchScreenBinding binding;
+    private MoviesAdapter moviesAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,17 +44,22 @@ public class SearchScreenFragment extends Fragment {
         View view;
         view= binding.getRoot();
         searchScreenViewModel= ViewModelProviders.of(this).get(SearchScreenViewModel.class);
-        this.initObservers();
-
+        this.initObservers();//*
+        initComponents();
 
         binding.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG,"onClick: "+ binding.etSearch.getText().toString());
-                observeModelData(binding.etSearch.getText().toString());
+                observeModelData(binding.etSearch.getText().toString());//*
             }
         });
             return view;
+    }
+
+    private void initComponents() {
+        moviesAdapter=new MoviesAdapter(this);
+        binding.rvSearchResult.setAdapter(moviesAdapter);
+
     }
 
     private void observeModelData(String movieName) {
@@ -59,21 +67,23 @@ public class SearchScreenFragment extends Fragment {
     }
 
     private void initObservers(){
+
         searchScreenViewModel.getMovieList().observe(getViewLifecycleOwner(), new Observer<List<MovieModel>>() {
             @Override
             public void onChanged(List<MovieModel> movieModels) {
-                Log.d(TAG, "onChanged: ");
-                //ekle
+                prepareRecycler(movieModels);
             }
         });
+    }
+
+    private void prepareRecycler(List<MovieModel> movieModels) {
+        moviesAdapter.setAdapterMovieList(movieModels);
     }
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate: Search Screen Fragment");
-
     }
 
 }
